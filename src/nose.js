d5f4029cb0b24bcff06144cfdccac07bdc5dbedb
Gloriafersof <Gloriafersof2013@gmail.com>
2023-05-15 12:23:16 -0400

    creando boton

---
+++ src/nose.js
@@ -0,0 +1,17 @@
+const button = document.getElementById("menu-hambur");
+const options= document.getElementById("options")
+console.log(button)
+
+button.addEventListener("click", () => {
+    button.classList.toggle("close");
+    options.classList.toggle("show");
+    console.log(options);
+});
+
+options.addEventListener("click", e =>{
+    if (nose.id === "options"){
+        options.classList.remove("show");
+        button.classList.remove("close")
+
+    }
+})
